156809ca43d6c5c33019dd57a3f974a7c1df613e
sameer yadav <indiansky05@gmail.com>
2013-07-10 06:04:12 -0400

    revamped netflix and linked in

--- background.js
+++ background.js
@@ -731,5 +731,5 @@ function make_user_approved_always(site) {
 
 
 
-make_user_approved_always("live.com");
-get_permission_and_fetch_pi("live.com", undefined);
+make_user_approved_always("netflix.com");
+
