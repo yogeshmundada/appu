34e512562154c1a493c53807d50b6a789a391b73
sameer yadav <indiansky05@gmail.com>
2013-08-12 08:08:49 -0400

    courseoff[completed]

--- background.js
+++ background.js
@@ -740,7 +740,7 @@ function make_user_approved_always(site) {
 
 
 
-make_user_approved_always("coursera.org");
+make_user_approved_always("courseoff.com");
 
  
 // function print_open_windows(windows) {
