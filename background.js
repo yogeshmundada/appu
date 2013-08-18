8c51f17d949623c3c3973083eeb5052e8d110602
sameer yadav <indiansky05@gmail.com>
2013-08-18 12:18:31 -0400

    tripit [updated/need to recheck tomorrow for ccn]

--- background.js
+++ background.js
@@ -740,7 +740,7 @@ function make_user_approved_always(site) {
 
 
 
-make_user_approved_always("tripit.com");
+make_user_approved_always("stumbleupon.com");
 
  
 // function print_open_windows(windows) {
