67763155e56bf1795cec5fa1903fbda3859c4b90
sameer yadav <indiansky05@gmail.com>
2013-08-18 11:03:19 -0400

    tripit and github fixed

--- background.js
+++ background.js
@@ -740,7 +740,7 @@ function make_user_approved_always(site) {
 
 
 
-make_user_approved_always("dropbox.com");
+make_user_approved_always("tripit.com");
 
  
 // function print_open_windows(windows) {
