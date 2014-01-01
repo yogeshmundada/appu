56cc0a10b521386f88df260d714429341686fd49
Yogesh Mundada <yogesh@mango.lan>
2014-01-01 13:21:18 -0500

    bloomfilter changes

--- downloads.js
+++ downloads.js
@@ -14,6 +14,24 @@
 // Should return currently present FPIs and
 // their versions.
 
+// Following 2 functions are from: http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+// I modified them to use Uint8Array instead of Uint16Array
+// Because of Uint8, these will work probably only for ASCII strings 
+function download_ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+function download_str2ab(str) {
+    var buf = new ArrayBuffer(str.length); 
+    var bufView = new Uint8Array(buf);
+
+    for (var i=0, strLen=str.length; i<strLen; i++) {
+	bufView[i] = str.charCodeAt(i);
+    }
+
+    return buf;
+}
+
 
 function print_downloaded_file(fname, ftype, dname) {
     var file_key = "Downloads:" + ftype;
@@ -26,6 +44,31 @@ function print_downloaded_file(fname, ftype, dname) {
     }
 
     read_from_local_storage(file_key, function(rc) {
+	    if (rc != undefined) {
+		var fzipped_data = rc[file_key];
+
+		unzipArrayBuffer(zipbuf, (function(file_key, file_data) {
+			    return function(uz_file_arrbuf) {
+				var uz_file_arrbuf_view8 = new Uint8Array(uz_file_arrbuf);
+				var uz_file_data = CryptoJS.enc.Latin1.parse(download_ab2str(uz_file_arrbuf_view8));
+			    
+				var cksum = CryptoJS.SHA1(uz_file_data).toString();
+				
+				if (file_data["file_sha1sum"] == cksum) {
+				    console.log("APPU DEBUG: Checksums MATCH for: '"+ file_key +"'");
+				    write_to_local_storage(file_data);
+				}
+				else {
+				    var err_str = "APPU Error: Checksums DO NOT match: '"+ file_key +"'"; 
+				    console.log(err_str);
+				    print_appu_error(err_str);
+				    return;
+				}
+			    }
+			})(file_key, file_data));
+	    }
+	    
+	    
 	    if (rc != undefined) {
 		if (dname == undefined) {
 		    console.log("APPU DEBUG: " + rc[file_key]);
@@ -76,6 +119,48 @@ function get_pwdbf_list() {
 }
 
 
+// Accepts a string and searches for it in the Uint8Array of an arrabuffer.
+// Once found, returns the starting location
+function search_in_arrayBuffer(srchstr, ab_view) {
+    var ab_view8 = new Uint8Array(ab_view);
+    var srchstr_view8 = new Uint8Array(download_str2ab(srchstr));
+
+    for (var i = 0; i < ab_view8.byteLength; i++) {
+	if (ab_view8[i] == srchstr_view8[0]) {
+	    for (var j = 0; j < srchstr_view8.byteLength; j++) {
+		if (ab_view8[i + j] == srchstr_view8[j]) {
+		    if (j == (srchstr_view8.byteLength - 1)) {
+			return [i, i + srchstr_view8.byteLength];
+		    }
+		}
+		else {
+		    break;
+		}
+	    }
+	}
+    }
+    return null;
+}
+
+// Accepts and tag and gets a string value for it in an arrayBuffer
+function extract_tag(tag, unzip_buf) {
+    var tag_start = "<" + tag + ">";
+    var tag_end = "</" + tag + ">";
+    var unzip_buf_view8 = new Uint8Array(unzip_buf);
+    var rc = search_in_arrayBuffer(tag_start, unzip_buf);
+    var start = rc[1];
+    var rc = search_in_arrayBuffer(tag_end, unzip_buf);
+    var end = rc[0];
+    var len = end - start;
+    var vab = new ArrayBuffer(len);
+    var vab_view8 = new Uint8Array(vab);
+
+    for (var i = 0; i < len; i++) {
+	vab_view8[i] = unzip_buf_view8[start + i];
+    }
+    return ab2str(vab);
+}
+
 // Download file
 function download_file(fname, ftype, dname) {
     var file_fetch_url = "http://192.168.56.101:59000/";
@@ -93,138 +178,95 @@ function download_file(fname, ftype, dname) {
     $.post(file_fetch_url + "get_file", 
 	   file_params,
 	   function(data) {
-	       var response = /^(Success\n)(.*)/.exec(data);
-
-	       if (response == null) {
-		   console.log("APPU Error: Error for: " + fname);
-		   console.log("APPU Error: Error message: " + data);
+	       var response = /^(OK\n)(.*)/.exec(data);
+	       var file_key = "Downloads:" + ftype;
+	       if (dname != undefined) {
+		   file_key = file_key + ":" + dname + ":" + fname;
 	       }
 	       else {
-		   console.log("APPU DEBUG: Success for: " + fname);
-		   var file_key = "Downloads:" + ftype;
-		   if (dname != undefined) {
-		       file_key = file_key + ":" + dname + ":" + fname;
+		   file_key = file_key + ":" + fname;
+	       }
+	       
+	       if (response == null) {
+		   var err_str = "APPU Error: Error downloading '"+ file_key +"', message: " + data; 
+		   console.log(err_str);
+		   print_appu_error(err_str);
+	       }
+	       else {
+		   console.log("APPU DEBUG: Downloaded successfully: '"+ file_key +"'");
+		   
+		   var data_wo_ok = data.split("OK\n")[1];
+		   var match = /^(Sha1sum: (.*)\n)?(.*)/.exec(data_wo_ok);
+		   
+		   if (match == undefined ||
+		       match[1] == undefined ||
+		       match[2] == undefined) {
+		       var err_str = "APPU Error: No sha1sum, corrupted data for: '"+ file_key +"'"; 
+		       console.log(err_str);
+		       print_appu_error(err_str);
 		   }
-		   else {
-		       file_key = file_key + ":" + fname;
-		   }
-
+		   
 		   var file_data = {};
-		   file_data[file_key] = data.split("Success\n")[1];
-		   write_to_local_storage(file_data);
-		   var f = file_data[file_key];
-		   var w = CryptoJS.enc.Base64.parse(f);
-		   //var bd = atob(w.toString(CryptoJS.enc.Base64));
-		   var bd = w.toString(CryptoJS.enc.Latin1);
-		   //console.log("Here here: bd(" + bd.length + "): " + sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(bd)));
 
-		   var buf1 = new ArrayBuffer(w.words.length * 4);
-		   var buf1view = new DataView(buf1);
+		   file_data["file_sha1sum"] = match[2];
+		   file_data[file_key] = match[3];
+		   var fdata = file_data[file_key];
 
-		   for (var k = 0; k < w.words.length; k++) {
-		       //buf1view[k] = w.words[k];
-		       buf1view.setInt32(k * 4, w.words[k], false);
+		   // Convert Base64 to binary words array
+		   var fdata_words = CryptoJS.enc.Base64.parse(fdata);
+
+		   // Convert binary basearray to arrayBuffer
+		   var fbuf = new ArrayBuffer(fdata_words.words.length * 4);
+		   var fbuf_view = new DataView(fbuf);
+
+		   for (var k = 0; k < fdata_words.words.length; k++) {
+		       // Taking care of little endianness
+		       fbuf_view.setInt32(k * 4, fdata_words.words[k], false);
 		   }
 
-		   var zip_buf = new ArrayBuffer(w.sigBytes);
-		   var zip_buf_view = new Uint8Array(zip_buf);
-		   var buf1view8 = new Uint8Array(buf1);
-		   var dtv = new DataView(buf1);
+		   // Since all words are aligned at the boundary of 4
+		   // need to create a new buffer to copy only filesize
+		   var zipbuf = new ArrayBuffer(fdata_words.sigBytes);
+		   var zipbuf_view = new Uint8Array(zipbuf);
+		   // Create 8-bits view on original words buffer
+		   var fbuf_view8 = new Uint8Array(fbuf);
 
-		   for (var k = 0; k < w.sigBytes; k++) {
-		       zip_buf_view[k] = buf1view8[k];
+		   // Copy only filesize to zipbif
+		   for (var k = 0; k < fdata_words.sigBytes; k++) {
+		       zipbuf_view[k] = fbuf_view8[k];
 		   }
 
-		   console.log("zip_buf_view: ");
-		   console.log(zip_buf_view);
-		   var array_buf = str2ab(bd);
+		   // Unzip the array buffer using zip.js library.
+		   unzipArrayBuffer(zipbuf, (function(file_key, file_data) {
+			       return function(uz_file_arrbuf) {
+				   var uz_file_arrbuf_view8 = new Uint8Array(uz_file_arrbuf);
+				   var uz_file_data = CryptoJS.enc.Latin1.parse(download_ab2str(uz_file_arrbuf_view8));
 
-		   var nv = new Uint8Array(zip_buf);
-		   var av = "";
-		   for (var k = 0; k < nv.length; k++) {
-		       var ap = "";
-		       if (nv[k] < 16) {
-			   ap = "0" + nv[k].toString(16);
-		       }
-		       else {
-			   ap = nv[k].toString(16);
-		       }
-		       av += ("," + ap);
-		   }
-		   console.log("NV: " + av);
+				   var version = extract_tag("version", uz_file_arrbuf);
+				   console.log("APPU DEBUG: Extracted version is: " + version);
 
-		   //var bb = new Blob([array_buf]);
-		   //var bb = new Blob([zip_buf], {type: 'application/octet-binary'});
-		   var bb = new Blob([zip_buf], {type: 'application/zip'});
-		   logBlobText(bb);
-		   unzipArrayBuffer(zip_buf, function(unzippedArrayBuffer) {
-			   logArrayBufferText(unzippedArrayBuffer);
-		       });
-// 		   unzipBlob(bb, function(unzippedBlob) {
-// 			   logBlobTextChecksum(unzippedBlob);
-// 		       });
+				   var num_setbits = extract_tag("setbits", uz_file_arrbuf);
+				   console.log("APPU DEBUG: Number of set bits: " + num_setbits);
 
-// 		   unzipArrayBuffer(array_buf, function(data) {
-// 			   logArrayBufferText(data);
-// 		       });
+				   var cksum = CryptoJS.SHA1(uz_file_data).toString();
+
+				   if (file_data["file_sha1sum"] == cksum) {
+				       console.log("APPU DEBUG: Checksums MATCH for: '"+ file_key +"'");
+				       file_data["version"] = version;
+				       file_data["setbits"] = num_setbits;
+				       write_to_local_storage(file_data);
+				   }
+				   else {
+				       var err_str = "APPU Error: Checksums DO NOT match: '"+ file_key +"'"; 
+				       console.log(err_str);
+				       print_appu_error(err_str);
+				       return;
+				   }
+			       }
+			   })(file_key, file_data));
 	       }
 	   });
 }
-
-// function zipBlob(blob, callback) {
-//     zip.createWriter(new zip.BlobWriter("application/zip"), function(zipWriter) {
-// 	    zipWriter.add(FILENAME, new zip.BlobReader(blob), function() {
-// 		    zipWriter.close(callback);
-//                 });
-//         }, onerror);
-// }
-
-// function zipBlob(blob, function(zippedBlob) {
-// 	logBlobText(zippedBlob);
-//     });
-
-
-
-// function unzipBlob(blob, callback) {
-//     zip.createReader(new zip.BlobReader(blob), function(zipReader) {
-// 	    zipReader.getEntries(function(entries) {
-// 		    entries[0].getData(new zip.BlobWriter(zip.getMimeType(entries[0].filename)), function(data) {
-// 			    zipReader.close();
-// 			    callback(data);
-//                         });
-//                 });
-// 	}, onerror);
-// }
-
-// function logBlobText(blob) {
-//     var reader = new FileReader();
-//     reader.onload = function(e) {
-// 	console.log(e.target.result);
-// 	console.log("--------------");
-//     };
-//     reader.readAsBinaryString(blob);
-// }
-
-
-// function unzipArrayBuffer(arrayBuffer, callback) {
-//     zip.createReader(new zip.ArrayBufferReader(arrayBuffer), function(zipReader) {
-// 	    zipReader.getEntries(function(entries) {
-// 		    entries[0].getData(new zip.ArrayBufferWriter(), function(data) {
-// 			    zipReader.close();
-// 			    callback(data);
-// 			});
-// 		});
-// 	}, onerror);
-// }
-
-// function logArrayBufferText(arrayBuffer) {
-//     var array = new Uint8Array(arrayBuffer);
-//     var str = "";
-//     Array.prototype.forEach.call(array, function(code) {
-// 	    str += String.fromCharCode(code);
-//         });
-//     console.log(str);
-// }
 
 
 // Delete
@@ -246,71 +288,7 @@ function read_file() {
 
 }
 
-function test_zip() {
-zip.createWriter(new zip.BlobWriter(), function(writer) {
-
-	// use a TextReader to read the String to add
-	var rd = new zip.TextReader("test!");
-	writer.add("filename.txt", rd, function() {
-		// onsuccess callback
-		console.log("here here");
-		// close the zip writer
-		writer.close(function(blob) {
-			// blob contains the zip file as a Blob object
-
-		    });
-	    }, function(currentIndex, totalIndex) {
-		// onprogress callback
-		console.log("here here");
-	    });
-    }, function(error) {
-	// onerror callback
-	console.log("here here");
-    });
-}
-
-// var blob = new Blob([ "Lorem ipsum dolor sit amet, consectetuer adipiscing elit..." ], {
-// 	type : "text/plain"
-//     });
-// // creates a zip storing the file "lorem.txt" with blob as data
-// // the zip will be stored into a Blob object (zippedBlob)
-// zipBlob("lorem.txt", blob, function(zippedBlob) {
-// 	logBlobTextBase64(zippedBlob);
-// 	// unzip the first file from zipped data stored in zippedBlob
-// 	unzipBlob(zippedBlob, function(unzippedBlob) {
-// 		// logs the uncompressed Blob
-// 		// console.log(unzippedBlob);
-// 		logBlobText(unzippedBlob);
-// 	    });
-//     });
-
-function logBlobText(blob) {
-    var reader = new FileReader();
-    reader.onloadend = function() {
-	var data = reader.result;
-	console.log(sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(data)));
-	var dv = new Uint8Array(data);
-	var nv = dv;
-// 	var av = "";
-// 	for (var k = 0; k < nv.length; k++) {
-// 	    var ap = "";
-// 	    if (nv[k] < 16) {
-// 		ap = "0" + nv[k].toString(16);
-// 	    }
-// 	    else {
-// 		ap = nv[k].toString(16);
-// 	    }
-// 	    av += ("," + ap);
-// 	}
-// 	console.log("NV: " + av);
-	
-	console.log(dv);
-	console.log(dv.length);
-    };
-    //reader.readAsBinaryString(blob);
-    reader.readAsArrayBuffer(blob);
-}
-
+// Following function is copied as it is from zip.js test files.
 function unzipArrayBuffer(arrayBuffer, callback) {
     zip.createReader(new zip.ArrayBufferReader(arrayBuffer), function(zipReader) {
 	    zipReader.getEntries(function(entries) {
@@ -322,29 +300,8 @@ function unzipArrayBuffer(arrayBuffer, callback) {
 	}, onerror);
 }
 
-function logArrayBufferText(arrayBuffer) {
-    var array = new Uint8Array(arrayBuffer);
-
-    var binary_data_str = CryptoJS.enc.Latin1.parse(ab2str(array));
-    var cksum = CryptoJS.SHA1(binary_data_str).toString();
-    console.log("APPU DEBUG: Unzipped data sha1 checksum: " + cksum);
-
-    var zzz = "";
-    for (var i = 0; i < array.length; i++) {
-	zzz += array[i].toString(16);
-    }
-	
-    console.log("--------------");
-
-    var str = "";
-    Array.prototype.forEach.call(array, function(code) {
-	    str += String.fromCharCode(code);
-        });
-    console.log(str);
-}
-
 function print_bits(arrayBuffer) {
-    var data = ab2str(arrayBuffer);
+    var data = download_ab2str(arrayBuffer);
     var bits = data.split("<bits>")[1].split("</bits>\n")[0];
     var ab = new ArrayBuffer(bits.length);
     for (var i = 0; i < ab.length; i++) {
@@ -378,89 +335,3 @@ function print_bits(arrayBuffer) {
     }
     console.log("Here here");
 }
-
-
-function logBlobTextChecksum(blob) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-	console.log(sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(e.target.result)));
-	console.log(e.target.result);
-	console.log("--------------");
-	var data = e.target.result;
-	var bits = data.split("<bits>")[1].split("</bits>\n")[0];
-	var ab = new ArrayBuffer(bits.length);
-	for (var i = 0; i < ab.length; i++) {
-	    ab[i] = bits[i];
-	}
-
-	var bit_set_array = [
-			     128,
-			     64,
-			     32,
-			     16,
-			     8,
-			     4,
-			     2,
-			     1,
-			     ];
-
-	var abv = new Uint8Array(ab);
-	for (var i = 0; i < abv.length; i++) {
-	    if (abv[i] == 0) {
-		continue;
-	    }
-	    else {
-		console.log("Found something");
-		for (var j = 0; j < 8; j++) {
-		    if (bit_set_array[j] & abv[i]) {
-			console.log("Here here: Bit " + j + " is set at byte " + i);
-		    }
-		}
-	    }
-	}
-	console.log("Here here");
-    };
-    reader.readAsBinaryString(blob);
-}
-
-
-function logBlobTextBase64(blob) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-	var w = CryptoJS.enc.Latin1.parse(e.target.result);
-	console.log(w.toString(CryptoJS.enc.Base64));
-	console.log("--------------");
-    };
-    reader.readAsBinaryString(blob);
-}
-
-function zipBlob(filename, blob, callback) {
-    // use a zip.BlobWriter object to write zipped data into a Blob object
-    zip.createWriter(new zip.BlobWriter("application/zip"), function(zipWriter) {
-	    // use a BlobReader object to read the data stored into blob variable
-	    zipWriter.add(filename, new zip.BlobReader(blob), function() {
-		    // close the writer and calls callback function
-		    zipWriter.close(callback);
-		});
-	}, onerror);
-}
-
-function unzipBlob(blob, callback) {
-    // use a zip.BlobReader object to read zipped data stored into blob variable
-    zip.createReader(new zip.BlobReader(blob), function(zipReader) {
-	    // get entries from the zip file
-	    zipReader.getEntries(function(entries) {
-		    // get data from the first file
-		    entries[0].getData(new zip.BlobWriter("text/plain"), function(data) {
-			    // close the reader and calls callback function with uncompressed data as parameter
-			    zipReader.close();
-			    callback(data);
-			});
-		});
-	}, onerror);
-}
-
-function onerror(message) {
-    console.error(message);
-}
-
